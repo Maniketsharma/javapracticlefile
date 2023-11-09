03d43338e6fe50aa6c29b9ad4afcccc55ab3bb69
Maniket sharma <sharmamaniket26@gmail.com>
2023-11-09 11:52:33 +0530

    Input Output prog5

---
+++ IO7104prog5.java
@@ -0,0 +1,15 @@
+public class IO7104prog5 {
+    public static void main(String[] args)throws java.lang.ClassNotFoundException {
+      java.util.Scanner scan = new java.util.Scanner(System.in);
+
+    System.out.print("Enter a double: ");
+    
+    
+    double d = scan.nextDouble();
+    System.out.println("Your enter "+d);
+    
+    scan.close();
+        
+        
+    }
+}
